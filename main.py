45c3b4cd2c68696a21e09a47e381de71cd6c921f
aosborne17 <AOsborne@spartaglobal.com>
2020-10-29 11:17:11 +0000

    Removing Threading From Code

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from amnesty_scrape import amnesty_scrape_func
 from cafod_scrape import cafod_scrape_func
 from oxfamsouthsudan_scrape import oxfam_scrape_func
-
+from threading import Thread
 
 
 if __name__ == "__main__":
-    # Thread(target = organisation_scrape).start()
     # Thread(target = cafod_scrape_func).start()
-    # Thread(target= amnesty_scrape_func).start()
+    # Thread(target= oxfam_scrape_func).start()
+
+    # Uncomment the function that you want to run, running them at the same time will sometimes lead to errors due to cookies disabling
     cafod_scrape_func()
-    # amnesty_scrape_func() # Additional Changes Needed
-    oxfam_scrape_func()
+    #oxfam_scrape_func()
